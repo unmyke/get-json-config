76710d021ec62953cdf8382f88ba8434d965796f
unmyke <unmyke@gmail.com>
2019-04-13 22:25:37 +0800

    feat(): return all configs if no scopeNames passed

--- src/extract-scopes.js
+++ src/extract-scopes.js
@@ -1,29 +1,49 @@
-module.exports = ({ scopeNames, scopes, env, dir }) =>
-  scopeNames.reduce(
-    ({ config: prevConfig, files: prevFiles }, scopeName) => {
-      let scope;
-      let newFiles;
-
-      if (!scopes[env] || !scopes[env][scopeName]) {
-        const message = !scopes[env]
-          ? `Environment ${env} doesn't declare in scope files`
-          : `Config file "${scopeName}" doesn't exist in directory "${dir}"`;
-        console.warn(`${message}. Return empty object.`);
-        scope = {};
-        newFiles = [...prevFiles];
-      } else {
-        const { value, file } = scopes[env][scopeName];
-        scope = value;
-        newFiles = [...prevFiles, file];
-      }
-
-      return {
-        config: {
-          ...prevConfig,
-          [scopeName]: scope,
-        },
-        files: newFiles,
-      };
+const getNewConfig = ({ scopes, env, scopeName, prevConfig, prevFiles }) => {
+  const { value, file } = scopes[env][scopeName];
+  return {
+    config: {
+      ...prevConfig,
+      [scopeName]: value,
     },
-    { config: {}, files: [] }
-  );
+    files: [...prevFiles, file],
+  };
+};
+
+module.exports = ({ scopeNames, scopes, env, dir }) =>
+  scopeNames
+    ? scopeNames.reduce(
+        ({ config: prevConfig, files: prevFiles }, scopeName) => {
+          if (!scopes[env] || !scopes[env][scopeName]) {
+            const message = !scopes[env]
+              ? `Environment ${env} doesn't declare in scope files`
+              : `Config file "${scopeName}" doesn't exist in directory "${dir}"`;
+            console.warn(`${message}. Return empty object.`);
+            return {
+              config: {
+                ...prevConfig,
+                [scopeName]: {},
+              },
+              files: prevFiles,
+            };
+          }
+          return getNewConfig({
+            scopes,
+            env,
+            scopeName,
+            prevConfig,
+            prevFiles,
+          });
+        },
+        { config: {}, files: [] }
+      )
+    : Object.keys(scopes[env]).reduce(
+        ({ config: prevConfig, files: prevFiles }, scopeName) =>
+          getNewConfig({
+            scopes,
+            env,
+            scopeName,
+            prevConfig,
+            prevFiles,
+          }),
+        { config: {}, files: [] }
+      );
